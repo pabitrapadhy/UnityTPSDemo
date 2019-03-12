b4c4896c235ef210b6888b84e725fedc55c57431
Pabitra Padhy <pabitrapadhycse@gmail.com>
2019-03-13 01:15:41 +0530

    Unity Third Person Character with Cinemachine

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 300f;
+    [SerializeField]
+    private float turnSpeed = 10f;
+    
+    private CharacterController character;
+    private Animator animator;
+    
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+        
+        // movement direction
+        var movement = new Vector3(horizontalAxis, 0, verticalAxis);
+
+        // get the character controller for simple movement
+        character.SimpleMove(movement * Time.deltaTime * speed);
+        
+        // set the look direction
+        if (movement.magnitude > 0)
+        {
+            // set the look direction
+            var lookDirection = Quaternion.LookRotation(movement);
+            
+            // slowly change the direction
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookDirection, 
+                Time.deltaTime * turnSpeed);    
+        }
+        
+        // set the blend animation idle <-> run
+        animator.SetFloat("Speed", movement.magnitude);
+    }
+}
